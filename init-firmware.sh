5a902a59eaba32ca025e47f21cf1ed418562159f
patrikx3 <alabard@gmail.com>
2023-08-09 14:30:21 +0200

    r0b08x [chore]   8/9/2023, 2:30:21 PM

--- init-firmware.sh
+++ init-firmware.sh
@@ -5,6 +5,7 @@ mkdir build-openwrt
 pushd build-openwrt
 wget https://downloads.openwrt.org/releases/${VERSION}/targets/mvebu/cortexa9/openwrt-imagebuilder-${VERSION}-mvebu-cortexa9.Linux-x86_64.tar.xz
 tar xf openwrt-imagebuilder-${VERSION}-mvebu-cortexa9.Linux-x86_64.tar.xz
+rm rf openwrt-imagebuilder-${VERSION}-mvebu-cortexa9.Linux-x86_64.tar.xz
 cp ../make-firmware.sh openwrt-imagebuilder-${VERSION}-mvebu-cortexa9.Linux-x86_64/
 pushd openwrt-imagebuilder-${VERSION}-mvebu-cortexa9.Linux-x86_64/
 ./make-firmware.sh
